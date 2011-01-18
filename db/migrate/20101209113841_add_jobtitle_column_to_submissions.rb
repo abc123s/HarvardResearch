b8051d216e80aabb9d0a32eb7b4c6f860ac499f7
Will <wjy.sun@gmail.com>
2011-01-18 17:02:59 -0500

    database restored

--- db/migrate/20101209113841_add_jobtitle_column_to_submissions.rb
+++ db/migrate/20101209113841_add_jobtitle_column_to_submissions.rb
@@ -1,7 +1,9 @@
 class AddJobtitleColumnToSubmissions < ActiveRecord::Migration
   def self.up
+    add_column :jobs, :title, :string
   end
 
   def self.down
+    remove_column :jobs, :title
   end
 end
